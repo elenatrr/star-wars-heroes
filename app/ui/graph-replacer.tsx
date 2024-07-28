efd6fd2e020adc02d4aabc1ad776a14d0f511468
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-07-28 20:01:09 +0100

    add an image to the graph-replacer

--- app/ui/graph-replacer.tsx
+++ app/ui/graph-replacer.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image"
+
 export default function GraphReplacer() {
-  return(
-    <div className="w-full bg-red-700 h-full rounded p-2">
-      <p>Click on a hero to see their details.</p>
+  return (
+    <div className="w-full bg-black h-full rounded overflow-hidden relative">
+      <Image
+        src="/graph-replacer-bg.webp"
+        width={600}
+        height={500}
+        className="w-full h-full object-cover object-right-top"
+        alt="Star Wars hero walking in the snow"
+      />
     </div>
   )
 }
