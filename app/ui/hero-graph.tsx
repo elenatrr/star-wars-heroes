87adc457249683cc25e9322f1d9f7508321c1e6b
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-07-29 19:03:22 +0100

    Revert "fix error handling in page.tsx and hero-graph.tsx"

--- app/ui/hero-graph.tsx
+++ app/ui/hero-graph.tsx
@@ -2,25 +2,11 @@
 import '@xyflow/react/dist/style.css';
 import { fetchFilmsByHeroId, fetchHeroById, fetchStarshipsByHeroId } from '../lib/data';
 import GraphNodes from './graph-nodes';
-import { Film, Hero, HeroFilmsData, HeroStarshipsData, Starship } from '../lib/definitions';
-import ErrorPage from '../error-page';
 
 export default async function HeroGraph({ heroId }: { heroId: number }) {
-  let hero: Hero | null = null;
-  let heroStarships: HeroStarshipsData | null = null
-  let heroFilms: HeroFilmsData | null = null
-
-  try {
-    hero = await fetchHeroById(heroId);
-    heroStarships = await fetchStarshipsByHeroId(heroId)
-    heroFilms = await fetchFilmsByHeroId(heroId);
-  } catch (error) {
-    console.error("Database Error:", error);
-  }
-
-  if (!hero || !heroFilms || !heroStarships) {
-    return <ErrorPage/>
-  }
+  const hero = await fetchHeroById(heroId);
+  const heroFilms = await fetchFilmsByHeroId(heroId);
+  const heroStarships = await fetchStarshipsByHeroId(heroId);
 
   return (
     <GraphNodes hero={hero} heroStarships={heroStarships} heroFilms={heroFilms} />
